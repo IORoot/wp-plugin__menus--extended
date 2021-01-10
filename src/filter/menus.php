75e2e31f960ad889fb735e0f6d1723ec5609309c
Andy Pearson <andy@londonparkour.com>
2021-01-10 14:44:54 +0000

    Running

--- src/filter/menus.php
+++ src/filter/menus.php
@@ -69,8 +69,11 @@ class code_inject
     public function menu_item_open($item_output, $item, $depth, $args)
     {
         
-        // vars
         $code = get_field('menu_item_open', $item);
+
+        // look for {{image}} or {{image_div}}
+        $code = $this->check_for_image_moustaches($code, $item);
+        $code = $this->check_for_image_div_moustaches($code, $item);
 
         if (!$code) {
             return $item_output;
@@ -138,6 +141,65 @@ class code_inject
         
         return $code;
 
+    }
+
+
+    // ┌─────────────────────────────────────────────────────────────────────────┐
+    // │                                                                         │
+    // │              check for {{image}} and replace with item image            │
+    // │                                                                         │
+    // └─────────────────────────────────────────────────────────────────────────┘ 
+    public function check_for_image_moustaches($code, $item)
+    {
+        $image = '';
+        
+
+        if (empty($code) || empty($item)){ return $code; }
+
+        if (strpos($code, '{{image}}') == false) { return $code; }
+
+        $image         = get_field('menu_image', $item);
+
+        if (empty($image)){ return $code; }
+
+        $image_classes = '';
+        $image_classes = get_field('menu_image_classes', $item);
+
+        $image_code = wp_get_attachment_image( $image['ID'], 'full', false, [ 'class' => $image_classes ] );
+
+        return str_replace('{{image}}', $image_code, $code);
+    }
+
+
+    // ┌─────────────────────────────────────────────────────────────────────────┐
+    // │                                                                         │
+    // │           check for {{image_div}} and replace with item image           │
+    // │                                                                         │
+    // └─────────────────────────────────────────────────────────────────────────┘ 
+    public function check_for_image_div_moustaches($code, $item)
+    {
+        $image = '';
+
+        if (empty($code) || empty($item)){ return $code; }
+
+        if (strpos($code, '{{image_div}}') == false) { return $code; }
+
+        $image = get_field('menu_image', $item);
+
+        if (empty($image)){ return $code; }
+
+        $image_classes = '';
+        $image_classes = get_field('menu_image_classes', $item);
+
+        $image_code = '<div';
+        $image_code .= ' class="' . $image_classes . '"';
+        $image_code .= ' data-bg="' . $image['url'] . '"';
+        $image_code .= ' alt="' . $image['alt'] . '"';
+        $image_code .= ' style="background-image: url(\'' . $image['url'] . '\'); "';
+        $image_code .= '>';
+        $image_code .= '</div>';
+
+        return str_replace('{{image_div}}', $image_code, $code);
     }
 }
 
